6b40fdd05f0fc5578063c99ba744691aad61248b
PARK SUNG DONG <oraedoa@gmail.com>
2020-01-06 17:47:27 +0900

    [refacor] 카프카 컨슈머 설정 내용 주석 작성.

--- pasudo123-spring-kafka-sample/src/test/java/edu/study/pasudo123/app/quick/QuickSimpleTest.java
+++ pasudo123-spring-kafka-sample/src/test/java/edu/study/pasudo123/app/quick/QuickSimpleTest.java
@@ -82,7 +82,7 @@ public class QuickSimpleTest {
 
     private KafkaMessageListenerContainer<Integer, String> createContainer(final ContainerProperties containerProperties) {
 
-        
+
         final DefaultKafkaConsumerFactory<Integer, String> kafkaConsumerFactory = new DefaultKafkaConsumerFactory<>(consumeProps());
 
         return new KafkaMessageListenerContainer<>(kafkaConsumerFactory, containerProperties);
@@ -99,16 +99,41 @@ public class QuickSimpleTest {
 
     private Map<String, Object> consumeProps() {
 
+        /**
+         * reference : https://memorynotfound.com/spring-kafka-consume-producer-example/ 설명글 참조.
+         * **/
+
         /** consumer 설정 **/
 
         final Map<String, Object> props = new HashMap<>();
 
+        // 카프카 클러스터 최초 연결에 대한 호스트 및 포트 목록을 지정. 해당 목록은 콤마로 구분되며 아래의 형식을 따른다.
+        // host1:port1,host2:port2 ...
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, LOCALHOST);
+
+        // 해당 컨슈머가 속한 컨슈머 그룹을 식별하는 고유한 문자열 설정
         props.put(ConsumerConfig.GROUP_ID_CONFIG, "foo_group");
+
+        // 자동 커밋 설정, 컨슈머는 poll() 을 호출할 시 가장 마지막 오프셋을 커밋.
         props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, true);
+
         props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "100");
         props.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, "15000");
+
+        // 단일 호출 poll() 에 대해서 최대 레코드 수를 지정.
+        props.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, 1);
+
+        // 카프카에 초기 오프셋이 없거나 서버에 현재 오프셋이 존재하지 않는 경우에 수행할 작업을 명시.
+        // - earliest : 가장 빠른 오프셋으로 자동설정
+        // - latest : 최신 오프셋으로 자동설정
+        // - none : 이전 오프셋이 없으면 에러 throws
+        // - anything else : 에러 throws
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
+
+        // 키 역직렬화 클래스 지정
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, IntegerDeserializer.class);
+
+        // 밸류 역직렬화 클래스 지정
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
 
         return props;
