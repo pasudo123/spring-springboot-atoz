fb6c1be3a93f2fa3fc7805b94037995eb52f5556
PARK SUNG DONG <oraedoa@gmail.com>
2020-02-07 14:47:23 +0900

    [init] 레디스 스트림 샘플.

---
+++ psaudo123-redis-stream-sample/src/main/java/edu/study/pasudo123/redisstream/RedisStreamApplication.java
@@ -0,0 +1,13 @@
+package edu.study.pasudo123.redisstream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisStreamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisStreamApplication.class, args);
+    }
+
+}
---
+++ psaudo123-redis-stream-sample/src/test/java/edu/study/pasudo123/redisstream/RedisStreamApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.study.pasudo123.redisstream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisStreamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
