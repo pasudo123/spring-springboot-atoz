a6c6e65c495148df2017fa4956a999dbad513f2f
PARK SUNG DONG <oraedoa@gmail.com>
2020-02-24 22:54:33 +0900

    [refactor] 도커 관련 연습.

---
+++ pasudo123-spring-docker-sample/src/test/java/edu/pasudo123/springdocker/SpringDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.pasudo123.springdocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDockerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
