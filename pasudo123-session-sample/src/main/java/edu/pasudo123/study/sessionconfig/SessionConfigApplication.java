bb54ca92adbf19e21f0d7d62d469729985992c96
PARK SUNG DONG <oraedoa@gmail.com>
2020-03-23 15:41:39 +0900

    [init]

---
+++ pasudo123-session-sample/src/main/java/edu/pasudo123/study/sessionconfig/SessionConfigApplication.java
@@ -0,0 +1,13 @@
+package edu.pasudo123.study.sessionconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SessionConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SessionConfigApplication.class, args);
+    }
+
+}
---
+++ pasudo123-session-sample/src/test/java/edu/pasudo123/study/sessionconfig/SessionConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.pasudo123.study.sessionconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SessionConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
