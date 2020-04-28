afbd018bcd83277ca11bb73be015a57d7997dfa5
PARK SUNG DONG <oraedoa@gmail.com>
2020-04-28 14:29:15 +0900

    [init]

---
+++ pasudo123-crawler-example/src/test/java/edu/pasudo123/study/crawler/CrawlerApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.pasudo123.study.crawler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrawlerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
